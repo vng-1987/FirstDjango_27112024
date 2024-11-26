6d55d43f3e34403339680848719634ccc493bc09
vng-1987 <vng-1987@mail.ru>
2024-11-27 00:27:42 +0400

    Done. Practice M1:task1

--- MainApp/views.py
+++ MainApp/views.py
@@ -1,3 +1,10 @@
 from django.shortcuts import render
+from django.http import HttpResponse
 
 # Create your views here.
+def home(request):
+	text = """
+		<h1>"Изучаем django"</h1>
+		<strong>Автор</strong>: <i>Иванов И.П.</i>
+	"""
+	return HttpResponse(text)
