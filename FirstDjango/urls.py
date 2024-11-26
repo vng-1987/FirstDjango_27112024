2925ca4d07f64a3ecb2b852bee2397742a51270b
vng-1987 <vng-1987@mail.ru>
2024-11-27 00:50:29 +0400

    Done. Practice M1:task2

--- FirstDjango/urls.py
+++ FirstDjango/urls.py
@@ -3,4 +3,5 @@ from MainApp import views
 
 urlpatterns = [
     path('', views.home),
+		path('about', views.about),
 ]
--- MainApp/views.py
+++ MainApp/views.py
@@ -1,10 +1,29 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 
+author = {
+	"Имя": "Иван",
+	"Отчество": "Петрович",
+	"Фамилия": "Иванов",
+	"телефон": "8-923-600-01-02",
+	"email": "vasya@mail.ru"
+}
+
 # Create your views here.
 def home(request):
 	text = """
 		<h1>"Изучаем django"</h1>
 		<strong>Автор</strong>: <i>Иванов И.П.</i>
+	"""
+	return HttpResponse(text)
+
+def about(request):
+	text = f"""
+		<h2>Информация об авторе</h2>
+		Имя: <b>{author['Имя']}</b><br>
+		Отчество: <b>{author['Отчество']}</b><br>
+		Фамилия: <b>{author['Фамилия']}</b><br>
+		Телефон: <b>{author['телефон']}</b><br>
+		Email: <b>{author['email']}</b><br>
 	"""
 	return HttpResponse(text)
