9b7c96b291b3176130c586f6ecd58e22df654aa4
vng-1987 <vng-1987@mail.ru>
2024-11-27 12:57:16 +0400

    Add context template example

--- MainApp/views.py
+++ MainApp/views.py
@@ -24,7 +24,11 @@ def home(request):
 # 		<strong>Автор</strong>: <i>Иванов И.П.</i>
 # 	"""
 # 	return HttpResponse(text)
-	return render(request, 'index.html')
+	context = {
+		'name': 'Петров Иван Николаевич',
+		'email': 'my_email@mail.ru'
+	}
+	return render(request, 'index.html', context)
 
 def about(request):
 	text = f"""
