f38bb704a7c3d9ce2353dfdff9a725de9ad84647
vng-1987 <vng-1987@mail.ru>
2024-11-28 23:27:33 +0400

    Done. Practice M3: Part1, task1-3

--- MainApp/models.py
+++ MainApp/models.py
@@ -6,3 +6,4 @@ class Item(models.Model):
    name  = models.CharField(max_length=100)
    brand = models.CharField(max_length=100)
    count = models.PositiveIntegerField()
+   description = models.TextField(max_length=200, default="Базовое описание товара")
